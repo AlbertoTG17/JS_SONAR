cd722d721d3f53d4238b61a6f724ec4acacf3bce
AlbertoTG17 <albertotg17@gmail.com>
2018-09-10 17:21:01 +0200

    wip

---
+++ src/main/resources/static/js/welcome.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+var x = 5;
+var y = 6
+
+
+function fecha(){
+	alert("Fecha: " + new Date());
+	console.log(x);
+}
+
+//98b45315df2897b8aae2112e66eee3fbe7e489e1
+
+/*sonar-scanner \
+-Dsonar.projectKey=Alberto17 \
+-Dsonar.sources=. \
+-Dsonar.host.url=http://172.17.0.2:9000 \
+-Dsonar.login=sdfasgsghh5g4fh86df7h4
